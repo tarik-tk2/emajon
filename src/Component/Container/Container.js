9d7ec2db37e307bbc83da3821157e4d1b1c460b8
mahim khan <mahimtk2@gmail.com>
2021-10-25 16:46:30 +0600

    diaplay

--- src/Component/Container/Container.js
+++ src/Component/Container/Container.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Header from '../Header/Header';
 import NabBar from '../NavBar/NabBar';
-
+import './Container.css'
 const Container = () => {
     return (
         <div>
            <Header/>
            <NabBar/>
+           <div className="display-section">
+               <div className="product">
+                 
+                 left
+               </div>
+               <div className="order-summary">
+                  right
+               </div>
+           </div>
         </div>
     );
 };
