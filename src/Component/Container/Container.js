b57e35c5e0f44728bfa78474af97b346883c45bf
mahim khan <mahimtk2@gmail.com>
2021-10-25 16:01:10 +0600

    logo added

--- src/Component/Container/Container.js
+++ src/Component/Container/Container.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../Header/Header';
 
 const Container = () => {
     return (
         <div>
-           hello from container 
+           <Header/>
         </div>
     );
 };
